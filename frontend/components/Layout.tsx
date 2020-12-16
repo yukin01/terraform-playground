5010475a29bb5cb56c6bbb48b7aa872433e705bc
yukin01 <38382781+yukin01@users.noreply.github.com>
2020-12-16 23:30:27 +0900

    Remove pages

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -19,15 +19,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         <Link href="/">
           <a>Home</a>
         </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
