9d8a407fec27deec7a5648230153ab3c6b6c9378
yukin01 <38382781+yukin01@users.noreply.github.com>
2020-12-18 00:13:43 +0900

    Handle tab in textarea

--- frontend/components/Editor.tsx
+++ frontend/components/Editor.tsx
@@ -1,8 +1,9 @@
-import React, { FC, DOMAttributes, useEffect } from 'react'
+import React, { FC, DOMAttributes, useEffect, useState } from 'react'
 import Prism from 'prismjs'
 
 type SetContent = React.Dispatch<React.SetStateAction<string>>
 type OnKeyDown = NonNullable<DOMAttributes<HTMLTextAreaElement>['onKeyDown']>
+type EventTargetElement = React.KeyboardEvent<HTMLTextAreaElement>['currentTarget']
 
 type Props = {
   language: string
@@ -10,9 +11,37 @@ type Props = {
   setContent: SetContent
 }
 
+// type NewTabHandler = (state: { target: EventTargetElement, position: number }) => () => void
+
+// const newTabHandler: NewTabHandler = (state) => {
+//   const { target, position } = state
+//   let done = false
+//   return () => {
+//     if (done) return
+//     target.selectionStart = position
+//     target.selectionEnd = position
+//     done = true
+//   }
+// }
+class TabHandler {
+  private done = false
+  constructor(private target: EventTargetElement, private position: number) {}
+  execute() {
+    if (this.done) return
+    this.target.selectionStart = this.position
+    this.target.selectionEnd = this.position
+    this.done = true
+  }
+}
+
 export const Editor: FC<Props> = ({ language, content, setContent }) => {
+  // const [handler, setHandler] = useState<ReturnType<NewTabHandler>>()
+  const [handler, setHandler] = useState<TabHandler>()
+
   useEffect(() => {
     Prism.highlightAll()
+    // handler && handler()
+    handler?.execute()
   }, [content])
 
   // handle 2-space indent on
@@ -21,15 +50,10 @@ export const Editor: FC<Props> = ({ language, content, setContent }) => {
     event.preventDefault()
 
     const { selectionStart, selectionEnd } = event.currentTarget
-    // console.log(event.currentTarget.selectionStart, event.currentTarget.selectionEnd)
-    // const position = event.currentTarget.selectionStart
-    // const value = content.substring(0, selectionStart) + '  ' + content.substring(selectionEnd)
-    // event.currentTarget.selectionStart = position + 2
 
-    // setContent(value)
     setContent(prev => prev.substring(0, selectionStart) + '  ' + prev.substring(selectionEnd))
-    // TODO: Update cursor after setContent
-    event.currentTarget.selectionStart = event.currentTarget.selectionEnd = selectionStart + 2
+    // setHandler(newTabHandler({ target: event.currentTarget, position: selectionStart + 2 }))
+    setHandler(new TabHandler(event.currentTarget, selectionStart + 2))
   }
 
   return (
@@ -39,6 +63,7 @@ export const Editor: FC<Props> = ({ language, content, setContent }) => {
         value={content}
         onChange={event => setContent(event.target.value)}
         onKeyDown={onKeyDown}
+        style={{ width: 1000, height: 200 }}
       />
       <pre className="code-output">
         <code className={`language-${language}`}>{content}</code>
